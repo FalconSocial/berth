2d7d1ba0365128db8732afca31018a31fefa688d
Jeppe Toustrup <jeppe@falconsocial.com>
2015-03-09 21:51:26 +0100

    Added --build-only and --package-only options. Fixes #4

--- berth/cli.py
+++ berth/cli.py
@@ -13,12 +13,18 @@ import click
 @click.argument('config_file', metavar='<CONFIG FILE>', type=click.File())
 @click.option('-v', '--verbose', is_flag=True, help='Turn on verbose output.')
 @click.option('-d', '--debug', is_flag=True, help='Turn on debug output.')
-def main(context, config_file, verbose, debug):
+@click.option('-b', '--build-only', is_flag=True, help='Only perform the build step.')
+@click.option('-p', '--package-only', is_flag=True, help='Only perform the package step.')
+def main(context, config_file, verbose, debug, build_only, package_only):  # pylint: disable = too-many-arguments
     """Build a package."""
     if debug:
         utils.set_log_level(2)
     elif verbose:
         utils.set_log_level(1)
+
+    if build_only and package_only:
+        utils.log('--build-only and --package-only are mutually exclusive.', err=True, fg='red', bold=True)
+        context.exit(1)
 
     configuration = config.read(config_file)
 
@@ -28,7 +34,11 @@ def main(context, config_file, verbose, debug):
     if not config.verify(configuration):
         context.exit(1)
 
-    if 'build' in configuration:
+    if 'build' not in configuration:
+        utils.info('No build configuration provided, skipping build.')
+    elif package_only:
+        utils.info('--package-only provided, skipping build.')
+    else:
         if not utils.pull_image(configuration['build']['image']):
             context.exit(1)
 
@@ -38,8 +48,11 @@ def main(context, config_file, verbose, debug):
     if not utils.pull_image(configuration['package'].get('image', 'dockerfile/fpm')):
         context.exit(1)
 
-    if not package.package(configuration):
-        context.exit(1)
+    if build_only:
+        utils.info('--build-only provided, skipping packaging.')
+    else:
+        if not package.package(configuration):
+            context.exit(1)
 
     utils.log('Done!')
     context.exit(0)
